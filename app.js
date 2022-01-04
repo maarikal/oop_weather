1f72fbab3e2a1578c08e2ae1d1196456fe242820
Maarika Lukk <maarika.lukk@khk.ee>
2022-01-04 16:38:01 +0200

    Get weatger data from API

---
+++ app.js
@@ -0,0 +1,13 @@
+// Weather object (kõik objektid on konstantsid)
+const weather = new Weather("Tallinn");
+
+// get city weather
+function  getWeather() {
+    weather.getWeather()
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => console.log(error))
+}
+
+getWeather()
---
+++ classes/Weather.js
@@ -0,0 +1,12 @@
+class Weather {
+    constructor(city) {
+        this.city = city;
+        this.key = "d43b9bc94518afe99cb9ba423d828c5b"
+    }
+
+    async getWeather() {
+        const response = await  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`);
+        const responseData = await response.json();
+        return responseData
+    }
+}
